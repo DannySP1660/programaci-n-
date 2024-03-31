2bfd11bad91b06acb293913aa17be4cb0ed6e63a
DannySP1660 <165528092+DannySP1660@users.noreply.github.com>
2024-03-30 23:27:47 -0500

    Add files via upload

---
+++ Trabajo 8 figuras.py
@@ -0,0 +1,34 @@
+import turtle
+import random
+
+def dibujar_poligono(tortuga, lados, color):
+    angulo = 360 / lados
+    tortuga.color(color)
+
+    for _ in range(lados):
+        tortuga.forward(100) 
+        tortuga.left(angulo) 
+
+def dibujar_figuras_desde_triangulo_a_octagono():
+    ventana = turtle.Screen()  
+    ventana.bgcolor("white")  
+    ventana.title("Dibujando polígonos")
+
+    tortuga = turtle.Turtle()
+    tortuga.shape("turtle")
+    tortuga.speed(2)
+
+    colores = ["red", "green", "blue", "orange", "purple", "yellow", "cyan", "magenta"]
+
+    for lados in range(3, 9):
+        tortuga.penup()
+        tortuga.setposition(0, 0)
+        tortuga.pendown()
+
+        
+        color = random.choice(colores)  
+        dibujar_poligono(tortuga, lados, color)
+
+    ventana.mainloop()
+
+dibujar_figuras_desde_triangulo_a_octagono()
